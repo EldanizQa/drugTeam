8ca1b23eb06d7b9e74b0952c693124b9da328d14
Skywalker <you@example.com>
2022-03-18 14:25:21 +0400

    yees

--- itex.js
+++ itex.js
@@ -1,2 +1,9 @@
-let name = "Rustam";
-console.log(name);
+function minimum(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+console.log(minimum(1, 10));
+console.log(minimum(-1, 0));
